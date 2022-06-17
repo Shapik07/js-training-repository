28c0aa9db86d98620fa207a7d0b542fc91694427
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-06-17 21:52:22 +0300

    Комит

--- js/test.js
+++ js/test.js
@@ -1,17 +1,4 @@
-let available = 10;
-let order = 5;
+"use strict";
 
-function checkStorage(available, ordered) {
-  let message;
-  // Change code below this line
-
-  if (ordered > available) {
-    message = "Not enough goods in stock!";
-  } else {
-    message = "The order is accepted, our manager will contact you";
-  }
-
-  // Change code above this line
-    return message;
-}
-
+console.log("Hello world!");
+console.log('Я учу JS')
