e33021b0d546624600c15fd5497400f533fa3339
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-06-17 22:33:05 +0300

    Пробные занятия

--- js/test.js
+++ js/test.js
@@ -2,3 +2,29 @@
 
 console.log("Hello world!");
 console.log("Я учу JS");
+
+//Изменение значения const
+
+const exchenge = {
+  name: "Ivan",
+  lastName: "Shapovalov",
+  age: 26,
+};
+
+console.log(exchenge);
+
+exchenge.age = 27;
+
+console.log(exchenge);
+
+
+// Попытка сделать ДЗ 
+
+const eye = 'blue';
+const user = 'Ivan';
+let userName = user;
+
+console.log(eye);
+console.log(user);
+console.log(userName);
+
