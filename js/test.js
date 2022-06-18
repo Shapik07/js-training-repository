03a9f8fdd0dd25596a6d24190f17b735d9db9e0c
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-06-18 15:34:29 +0300

    проверка

--- js/test.js
+++ js/test.js
@@ -15,4 +15,13 @@ console.log("Я учу JS");
 
 // exchenge.age = 27;
 
-// проверка
+const number = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+const mac = 14;
+for (let i = 0; i < number.length; i += 1) {
+  if (number[i] < mac) {
+    continue
+  }
+  console.log(`Число больше чем ${mac} : ${number[i]}`);
+}
