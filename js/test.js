7a404ff87ee58e2893aab4946e4c1e682db189c6
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-06-18 21:57:07 +0300

    commit

--- js/test.js
+++ js/test.js
@@ -14,3 +14,15 @@ console.log("Я учу JS");
 // console.log(exchenge);
 
 // exchenge.age = 27;
+
+
+const numbers = [56, 75, 80, 34, 30, 25, 56, 17, 80, 6, 17, 50];
+let smallestNumber = numbers[0];
+
+for (const number of numbers) {
+    if (number < smallestNumber) {
+        smallestNumber = number;
+    }
+}
+
+console.log(smallestNumber)
