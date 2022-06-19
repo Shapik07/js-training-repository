7722bfbcedf0b526af17f965e1362066d72fcc81
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-06-19 21:55:24 +0300

    Исправляет ошибки

--- js/test.js
+++ js/test.js
@@ -15,18 +15,3 @@ console.log("Я учу JS");
 
 // exchenge.age = 27;
 
-function getExtremeElements(array) {
-  const newArray = [];
-  for (let i = 0; i < array.length; i += 1) {
-    if ((i = array[0])) {
-      const firstElement = array[0];
-      console.log(firstElement);
-    }
-
-    // console.log(firs);
-  }
-}
-
-console.log(getExtremeElements([1, 2, 3, 4, 5]));
-console.log(getExtremeElements(["Earth", "Mars", "Venus"]));
-console.log(getExtremeElements(["apple", "peach", "pear", "banana"]));
