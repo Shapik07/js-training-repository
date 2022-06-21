6e4603cf03bd3b29e43836709f4e4f03a39f60cf
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-06-21 17:54:12 +0300

    Commit

--- js/test.js
+++ js/test.js
@@ -15,20 +15,54 @@ console.log("Я учу JS");
 
 // exchenge.age = 27;
 
-function getCommonElements(array1, array2) {
-  let newArray = [];
+// function getCommonElements(array1, array2) {
+//   let newArray = [];
 
-  for (let i = 0; i < array1.length; i += 1) {
-    for (let j = 0; j < array2.length; j += 1) {
-      if (array1[i] === array2[j]) {
-        array2[j].includes;
-        newArray.push(array2[j]);
-      }
-    }
-  }
-  return newArray;
-}
+//   for (let i = 0; i < array1.length; i += 1) {
+//     for (let j = 0; j < array2.length; j += 1) {
+//       if (array1[i] === array2[j]) {
+//         array2[j].includes;
+//         newArray.push(array2[j]);
+//       }
+//     }
+//   }
+//   return newArray;
+// }
 
 
-getCommonElements([1, 2, 3], [2, 4]);
+// getCommonElements([1, 2, 3], [2, 4]);
 
+
+
+
+// ЗАДАЧА 21 (МОДУЛЬ 2)
+
+// function findLongestWord(string) {
+
+//   const array = string.split(" ");
+//   let longestWord = [0];
+
+//   for (let i = 0; i <= array[i].length; i += 1) {
+//     if (array[i].length > longestWord.length) {
+//       longestWord = array[i]
+//     }
+//   } console.log(longestWord);
+// }
+
+
+
+// const findLongestWord = function (str) {
+//   let arrStr = str.split(" ");
+//   let wordLength = 0;
+//   let longestWord;
+
+//   for (let i = 0; i < arrStr.length; i += 1) {
+//     wordLength = arrStr[1].length;
+
+//     if (arrStr[i].length > wordLength) {
+//       longestWord = arrStr[i];
+
+//       return longestWord;
+//     }
+//   }
+// };
