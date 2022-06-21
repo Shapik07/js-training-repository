edec4ec818422028a77d1748097020ba87f3728c
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-06-21 22:01:28 +0300

    Удаляет лишние элементы

--- js/test.js
+++ js/test.js
@@ -15,41 +15,9 @@ console.log("Я учу JS");
 
 // exchenge.age = 27;
 
-// function getCommonElements(array1, array2) {
-//   let newArray = [];
-
-//   for (let i = 0; i < array1.length; i += 1) {
-//     for (let j = 0; j < array2.length; j += 1) {
-//       if (array1[i] === array2[j]) {
-//         array2[j].includes;
-//         newArray.push(array2[j]);
-//       }
-//     }
-//   }
-//   return newArray;
-// }
 
 
 // getCommonElements([1, 2, 3], [2, 4]);
-
-
-
-
-// ЗАДАЧА 21 (МОДУЛЬ 2)
-
-// function findLongestWord(string) {
-
-//   const array = string.split(" ");
-//   let longestWord = [0];
-
-//   for (let i = 0; i <= array[i].length; i += 1) {
-//     if (array[i].length > longestWord.length) {
-//       longestWord = array[i]
-//     }
-//   } console.log(longestWord);
-// }
-
-
 
 // const findLongestWord = function (str) {
 //   let arrStr = str.split(" ");
