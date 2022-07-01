743b19af763b26d6bf56b61a2743728876dc0477
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-01 09:41:46 +0300

    Добавляет задачу

--- js/test.js
+++ js/test.js
@@ -28,15 +28,11 @@ console.log("Я учу JS");
 
 // console.log(calcSum(4, 3));
 
-const numbers = [1, 3, 14, 18, 4, 7, 29, 6, 34];
-const threshold = 15;
-
-// Для чисел меньше чем порог срабатывает continue, выполнение тела прекращается
-// и управление передаётся на следующую итерацию.
-for (let i = 0; i < numbers.length; i += 1) {
-  if (numbers[i] < threshold) {
-    continue;
-  }
-
-  console.log(`Число больше чем ${threshold}: ${numbers[i]}`); // 18, 29, 34
-}
+const apartment = {
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+};
+// Change code below this line
+const keys = Object.keys(apartment);
+const values = Object.values(apartment);
