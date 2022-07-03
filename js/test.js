6aee29a12d7ae3999ebe6fea6a7d86050f55253f
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-03 17:18:18 +0300

    commit

--- js/test.js
+++ js/test.js
@@ -30,6 +30,12 @@ console.log("Я учу JS");
 
 
 
+
+
+
+
+
+
 const products = [
   { name: "Radar", price: 1300, quantity: 4 },
   { name: "Scanner", price: 2700, quantity: 3 },
@@ -37,37 +43,39 @@ const products = [
   { name: "Grip", price: 1200, quantity: 9 },
 ];
 
+
 function getAllPropValues(propName) {
-  let newArray = [];
+  let newArray = []
+  let newArrayName = [];
+  let newArrayPrice = [];
+  let newArrayQuantity = [];
   for (const product of products) {
     if (product.name === propName) {
-      newArray.push(product.name);
+      newArrayName.push(product.name)
+        return newArrayName
+    } else if (product.price === propName) {
+      newArrayPrice.push(product.price)
+      return newArrayPrice
+    } else if (product.quantity === propName) {
+      newArrayQuantity.push(product.quantity)
+      return newArrayQuantity
+    } else {
+      return newArray
     }
+   }
   }
-}
 
 
-const keys = Object.keys(products)
-console.log(keys)
-for (const key of keys) {
-  console.log(key)
-  console.log(products[key])
-}
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
 
-
-//  let newArray = [];
-//  for (const product of products) {
-//    newArray.push(product.name);
-//  }
-
-
-
-  // let newArray = [];
-  // for (const product of products) {
-  //   if (product.key === propName) {
-  //     newArray.push(product[key]);
-  //   }
-  // }
-
-  // console.log(newArray)
-
+// for (const key in book) {
+//   // Ключ
+//   console.log(key);
+//   // Значение свойства с таким ключом
+//   console.log(book[key]);
+// }
