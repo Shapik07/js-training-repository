0eb18c36eafee71d591d1cb274cc6d14b1c2b78f
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-05 11:40:47 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -29,22 +29,18 @@ console.log("Я учу JS");
 
 
 
-// Change code below this line
-function findMatches(firstElement, ...args) {
-  const matches = []; // Don't change this line
-  const newArray = [...firstElement, ...args];
-  // console.log(newArray)
-  for (let i = 0; i <= newArray.length; i += 1) {
-    for (let j = 0; j < i; j += 1) {
-      if (newArray[j] === newArray[i]) {
-        matches.push(newArray[j]);
-      }
-    }
-  }
-  console.log(matches);
-}
-
-findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7);
-findMatches([4, 89, 17, 36, 2], 8, 17, 89, 27, 2);
-findMatches([10, 24, 41, 6, 9, 19], 24, 11, 9, 23, 41);
-findMatches([63, 11, 8, 29], 4, 7, 16);
+const bookShelf = {
+  books: ["The last kingdom", "The guardian of dreams"],
+  getBooks() {
+    return "Returning all books";
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  removeBook() {
+    return "Deleting book Red sunset";
+  },
+  updateBook(bookName) {
+    return `Updating book ${bookName} to Dune`;
+  },
+};
