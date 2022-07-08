343ffce5ebf2363cd9b471097d612c18edc709bb
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-08 14:53:01 +0300

    commit

--- js/test.js
+++ js/test.js
@@ -28,15 +28,43 @@ console.log("Я учу JS");
 
 
 
-const students = [
+const studentss = [
   { name: "Манго", courses: ["математика", "физика"] },
   { name: "Поли", courses: ["информатика", "математика"] },
   { name: "Киви", courses: ["физика", "биология"] },
 ];
 
-const arr1 = students.map(student => student.courses)
-const arr2 = students.flatMap(student => student.courses)
+const allCourses = studentss.flatMap(student => student.courses);
+// ['математика', 'физика', 'информатика', 'математика', 'физика', 'биология'];
 
-console.log(arr1)
-console.log(arr2)
+const uniqueCourses = allCourses.filter(
+  (course, index, array) => array.indexOf(course) === index
+);
+
+console.log(uniqueCourses)
+
+
+
+
+const LOW_SCORE = 50;
+const HIGH_SCORE = 80;
+const students = [
+  { name: "Манго", score: 83 },
+  { name: "Поли", score: 59 },
+  { name: "Аякс", score: 37 },
+  { name: "Киви", score: 94 },
+  { name: "Хьюстон", score: 64 },
+];
+
+const best = students.filter(student => student.score >= HIGH_SCORE);
+console.log(best); // Массив объектов с именами Манго и Киви
+
+const worst = students.filter(student => student.score < LOW_SCORE);
+console.log(worst); // Массив с одним объектом Аякс
+
+// В коллбек-функции удобно деструктуризировать свойства объекта
+const average = students.filter(
+  ({score}) => score >= LOW_SCORE && score < HIGH_SCORE
+);
+console.log(average); // Массив объектов с именами Поли и Хьюстон
 
