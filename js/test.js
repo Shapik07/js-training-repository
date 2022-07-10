0be15e95262e89c88d6462d86f62fb33a2a4e3d8
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-10 09:01:22 +0300

    Commit

--- js/test.js
+++ js/test.js
@@ -27,21 +27,87 @@ console.log("Я учу JS");
 
 
 
-
-function changeEven(numbers, value) {
-  // Change code below this line
-  const newArray = [];
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] % 2 === 0) {
-      newArray.push(numbers[i] + value);
-    }
-  }
-  console.log(newArray);
-  // Change code above this line
-}
-
-
-
-changeEven([1, 2, 3, 4, 5], 10);
-changeEven([2, 8, 3, 7, 4, 6], 10);
-
+const getUserNames = (users) => {
+  const names = users.map((user) => user.name);
+  return names;
+};
+  
+ const users = [
+   {
+     name: "Moore Hensley",
+     email: "moorehensley@indexia.com",
+     eyeColor: "blue",
+     friends: ["Sharron Pace"],
+     isActive: false,
+     balance: 2811,
+     skills: ["ipsum", "lorem"],
+     gender: "male",
+     age: 37,
+   },
+   {
+     name: "Sharlene Bush",
+     email: "sharlenebush@tubesys.com",
+     eyeColor: "blue",
+     friends: ["Briana Decker", "Sharron Pace"],
+     isActive: true,
+     balance: 3821,
+     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+     gender: "female",
+     age: 34,
+   },
+   {
+     name: "Ross Vazquez",
+     email: "rossvazquez@xinware.com",
+     eyeColor: "green",
+     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+     isActive: false,
+     balance: 3793,
+     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+     gender: "male",
+     age: 24,
+   },
+   {
+     name: "Elma Head",
+     email: "elmahead@omatom.com",
+     eyeColor: "green",
+     friends: ["Goldie Gentry", "Aisha Tran"],
+     isActive: true,
+     balance: 2278,
+     skills: ["adipisicing", "irure", "velit"],
+     gender: "female",
+     age: 21,
+   },
+   {
+     name: "Carey Barr",
+     email: "careybarr@nurali.com",
+     eyeColor: "blue",
+     friends: ["Jordan Sampson", "Eddie Strong"],
+     isActive: true,
+     balance: 3951,
+     skills: ["ex", "culpa", "nostrud"],
+     gender: "male",
+     age: 27,
+   },
+   {
+     name: "Blackburn Dotson",
+     email: "blackburndotson@furnigeer.com",
+     eyeColor: "brown",
+     friends: ["Jacklyn Lucas", "Linda Chapman"],
+     isActive: false,
+     balance: 1498,
+     skills: ["non", "amet", "ipsum"],
+     gender: "male",
+     age: 38,
+   },
+   {
+     name: "Sheree Anthony",
+     email: "shereeanthony@kog.com",
+     eyeColor: "brown",
+     friends: ["Goldie Gentry", "Briana Decker"],
+     isActive: true,
+     balance: 2764,
+     skills: ["lorem", "veniam", "culpa"],
+     gender: "female",
+     age: 39,
+   },
+ ];
