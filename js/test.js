3ed3af61855a5138b4b55d43c2909484c26b5475
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-14 16:14:32 +0300

    Commit

--- js/test.js
+++ js/test.js
@@ -13,6 +13,27 @@ const user = {
 
 user.showTag()
 
-const outerShowTag = user.showTag
 
-user.outerShowTag
+
+class User {
+  // Синтаксис объявления метода класса
+  constructor(name, email) {
+    // Инициализация свойств экземпляра
+    this.name = name;
+    this.email = email;
+  }
+}
+
+const mango = new User("Манго", "mango@mail.com");
+console.log(mango); // { name: 'Манго', email: 'mango@mail.com' }
+
+const poly = new User("Поли", "poly@mail.com");
+console.log(poly); // { name: 'Поли', email: 'poly@mail.com' }
+
+
+console.log(poly.email)
+console.log(mango.name)
+
+
+
+
