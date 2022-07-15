66b72e50705be22f0594c4e756f4f10f9fcac8ec
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-15 10:15:39 +0300

    Commit

--- js/test.js
+++ js/test.js
@@ -3,36 +3,25 @@
 console.log("Hello world!");
 console.log("Я учу JS");
 
-const user = { 
-  tag: 'Mango',
-  showTag() {
-    console.log('showTag -> this', this)
-    console.log('showTag -> this.tag', this.tag)
-  }
-}
-
-user.showTag()
 
 
+const hotel = {
+  username: "Resort hotel",
+  showThis() {
+    const foo = () => {
+      // Стрелки запоминают контекст во время объявления,
+      // из родительской области видимости
+      console.log("this in foo: ", this);
+    };
 
-class User {
-  // Синтаксис объявления метода класса
-  constructor(name, email) {
-    // Инициализация свойств экземпляра
-    this.name = name;
-    this.email = email;
-  }
-}
+    foo();
+    console.log("this in showThis: ", this);
+  },
+};
 
-const mango = new User("Манго", "mango@mail.com");
-console.log(mango); // { name: 'Манго', email: 'mango@mail.com' }
-
-const poly = new User("Поли", "poly@mail.com");
-console.log(poly); // { name: 'Поли', email: 'poly@mail.com' }
-
-
-console.log(poly.email)
-console.log(mango.name)
+hotel.showThis();
+// this in foo: {username: 'Resort hotel', showThis: ƒ}
+// this in showThis: {username: 'Resort hotel',showThis: ƒ}
 
 
 
