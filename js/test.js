d7a007c07e469ab3c86db57be662d8db9f01b08b
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-07-17 20:54:16 +0300

    Добавляет решение всех задач 5 модуля

--- js/test.js
+++ js/test.js
@@ -5,31 +5,16 @@ console.log("Я учу JS");
 
 
 
-class Storage {
-
-    getItems() {
-        return this.items
-    };
-
-    addItem(newItem) { 
-        this.items.push(newItem)
-    };
-
-      removeItem(itemToRemove) {
-      const deleted = this.items.indexOf(itemToRemove)
-      this.items.splice(deleted, 1)
-    };
-    
+const objC = {
+    z: 5
 }
 
+console.log(objC)
+console.log(objC.z)
+console.log(objC.hasOwnProperty('z'))
 
-// Change code above this line
-const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
-storage.addItem("Droid");
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
-storage.removeItem("Prolonger");
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+
 
 
 
