a782b4ff40a50564571e65fe65b198d93c5073cb
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-08-01 14:55:34 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -4,22 +4,26 @@ console.log("Hello world!");
 console.log("Я учу JS");
 
 
-const result = {
-  success: ["max-length", "no-amd", "prefer-arrow-functions"],
-  failure: ["no-var", "var-on-top", "linebreak"],
-  skipped: ["no-extra-semi", "no-dup-keys"],
-};
 
-
-function makeList(arr) {
-  // Only change code below this line
-  const failureItems = [];
-  for (const value of arr) {
-    failureItems.push(`<li class="text-warning">${value}</li>`);
+class Thermostat {
+  constructor(fahrenheit) {
+    this.fahrenheit = fahrenheit;
   }
-  // Only change code above this line
 
-  return failureItems;
+  get temperature() {
+    return (5 / 9) * (this.fahrenheit - 32);
+  }
+
+  set temperature(celsius) {
+    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  }
 }
 
-const failuresList = makeList(result.failure);
+
+
+
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
