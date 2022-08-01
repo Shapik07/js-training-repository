fba40dcc8386e7979a9dcce768bbea06ddd7e30e
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-08-01 11:01:38 +0300

    update

--- js/test.js
+++ js/test.js
@@ -7,16 +7,17 @@ console.log("Я учу JS");
 const result = {
   success: ["max-length", "no-amd", "prefer-arrow-functions"],
   failure: ["no-var", "var-on-top", "linebreak"],
-  skipped: ["no-extra-semi", "no-dup-keys"]
+  skipped: ["no-extra-semi", "no-dup-keys"],
 };
 
 
 function makeList(arr) {
-  const failureItems = [
-    `<li class="text-warning">${arr}</li>`,
-    `<li class="text-warning">${arr}</li>`,
-    `<li class="text-warning">${arr}</li>`,
-  ];
+  // Only change code below this line
+  const failureItems = [];
+  for (const value of arr) {
+    failureItems.push(`<li class="text-warning">${value}</li>`);
+  }
+  // Only change code above this line
 
   return failureItems;
 }
