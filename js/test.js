76a33462a277666e0ae9189d390d2e2bae6fa497
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-08-01 22:31:31 +0300

    Uppdate

--- js/test.js
+++ js/test.js
@@ -3,27 +3,18 @@
 console.log("Hello world!");
 console.log("Я учу JS");
 
+function getGrade(s1, s2, s3) {
+  let total = (s1 + s2 + s3) / 3;
 
-
-class Thermostat {
-  constructor(fahrenheit) {
-    this.fahrenheit = fahrenheit;
-  }
-
-  get temperature() {
-    return (5 / 9) * (this.fahrenheit - 32);
-  }
-
-  set temperature(celsius) {
-    this.fahrenheit = (celsius * 9.0) / 5 + 32;
+  if (total >= 90 && total <= 100) {
+    return "A";
+  } else if (total >= 80 && total < 90) {
+    return "B";
+  } else if (total >= 70 && total < 80) {
+    return "C";
+  } else if (total >= 60 && total < 70) {
+    return "D";
+  } else {
+    return "F";
   }
 }
-
-
-
-
-
-const thermos = new Thermostat(76); // Setting in Fahrenheit scale
-let temp = thermos.temperature; // 24.44 in Celsius
-thermos.temperature = 26;
-temp = thermos.temperature; // 26 in Celsius
