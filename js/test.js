73919c41b84795446d8b1637ce34cc8c9c175574
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-08-01 10:28:50 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -4,46 +4,21 @@ console.log("Hello world!");
 console.log("Я учу JS");
 
 
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
 
-const contacts = [
-  {
-    firstName: "Akira",
-    lastName: "Laine",
-    number: "0543236543",
-    likes: ["Pizza", "Coding", "Brownie Points"],
-  },
-  {
-    firstName: "Harry",
-    lastName: "Potter",
-    number: "0994372684",
-    likes: ["Hogwarts", "Magic", "Hagrid"],
-  },
-  {
-    firstName: "Sherlock",
-    lastName: "Holmes",
-    number: "0487345643",
-    likes: ["Intriguing Cases", "Violin"],
-  },
-  {
-    firstName: "Kristian",
-    lastName: "Vos",
-    number: "unknown",
-    likes: ["JavaScript", "Gaming", "Foxes"],
-  },
-];
 
-function lookUpProfile(name, prop) {
-    for (let i = 0; i < contacts.length; i += 1) {
-      if (contacts[i].firstName === name) {
-        if (contacts[i].hasOwnProperty(prop)) {
-          console.log(contacts[i][prop]);
-        } else {
-          console.log("No such property");
-        }
-        } 
-    }
-    console.log("No such contact");
+function makeList(arr) {
+  const failureItems = [
+    `<li class="text-warning">${arr}</li>`,
+    `<li class="text-warning">${arr}</li>`,
+    `<li class="text-warning">${arr}</li>`,
+  ];
+
+  return failureItems;
 }
 
-
-lookUpProfile("Bob", "number");
+const failuresList = makeList(result.failure);
