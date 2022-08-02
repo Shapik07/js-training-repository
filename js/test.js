d556383636541754c89b8723c90575f86ececaef
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-08-02 13:24:32 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -3,17 +3,11 @@
 console.log("Hello world!");
 console.log("Я учу JS");
 
-function sumMix(x) {
-  let total = 0;
+const str = "How can mirrors be real if our eyes aren't real";
 
-  for (const num of x) {
-    let number = Number(num)
-    total += number
-  }
-   return total;
+function myStr() {
+  this.split(' ').map(function (word) {
+    return word.charAt(0).toUpperCase() + word.slice(1)
+  }).join(' ');
 }
 
-
-    sumMix([9, 3, "7", "3"]);
-    sumMix(["5", "0", 9, 3, 2, 1, "9", 6, 7]);
-    sumMix(["3", 6, 6, 0, "5", 8, 5, "6", 2, "0"]);
