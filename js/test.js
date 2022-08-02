22515a9e2a55f29469c3d7937094f835f0ac1f82
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-08-02 14:22:23 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -3,11 +3,14 @@
 console.log("Hello world!");
 console.log("Я учу JS");
 
-const str = "How can mirrors be real if our eyes aren't real";
-
-function myStr() {
-  this.split(' ').map(function (word) {
-    return word.charAt(0).toUpperCase() + word.slice(1)
-  }).join(' ');
+function sumTwoSmallestNumbers(numbers) {
+  const numOne = Math.min(...numbers);
+  console.log(numOne);
+  const indexNumOne = numbers.indexOf(numOne);
+  console.log(indexNumOne);
+  const newArray = numbers.splice(indexNumOne, 1);
+  const numTwo = Math.min(...numbers);
+  console.log(numOne + numTwo);
 }
 
+sumTwoSmallestNumbers([5, 8, 12, 19, 22]);
