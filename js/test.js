472d7fa1b8758a8f95f4c913a2e9bae3fd5ce4ed
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-08-05 17:44:48 +0300

    Upldate

--- js/test.js
+++ js/test.js
@@ -1,42 +1,16 @@
 "use strict";
 
-// function nbYear(p0, percent, aug, p) {
-//   let element = p0;
-//   let total = 0;
-//   let ourPercent = percent / 100;
-//   let step = p0 * ourPercent + aug;
-//   for (element = p0; element < p; element += step) {
-//     total += 1;
-//   }
-//   console.log(total);
-// }
-
-// function nbYear(p0, percent, aug, p) {
-
-//   let total = 0;
-//   let ourPercent = percent / 100;
-//   for (var p0; p0 <= p; p0 += p0 * ourPercent + aug) {
-//     total += 1;
-//   }
-//   console.log(total);
-// }
-
-function nbYear(p0, percent, aug, p) {
-  let element = p0;
-  let ourPercent = percent / 100;
-
-  let total = 0;
-  let step = () => element * ourPercent + aug;
-  for (/* no-op */; element < p; element += step()) {
-    total += 1;
+function squareDigits(num) {
+  let newSum = [];
+  const string = String(num)
+  const number = string.split('')
+  for (const element of number) {
+    newSum.push(Math.pow(element, 2));
   }
-  console.log("total", total);
-  return total;
+  const result = Number(newSum.join(""));
+  return result
 }
 
 
-
-
-nbYear(1500, 5, 100, 5000); // 15
-nbYear(1500000, 2.5, 10000, 2000000); // 10
-nbYear(1500000, 0.25, 1000, 2000000); // 94
+squareDigits(3212); // 9414
+squareDigits(2112); // 4114
