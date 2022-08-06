bfbb59a66c38643547a105b347da79f4f1428e09
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-08-06 17:39:39 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -1,11 +1,16 @@
 "use strict";
 
-function reverseWords(str) {
-  const newStr = str.split(" ").map(function (a) {
-    return a.split("").reverse().join('')
-  }).join(' ')
+function sumArray(array) {
+  let total = 0;
+  const tallerNumber = Math.max(...array);
+  const smallerNumber = Math.min(...array)
 
-  console.log(newStr);
+  for (const element of array) {
+    total += element;
+  }
+  console.log(total - tallerNumber - smallerNumber);
 }
 
-reverseWords("The quick brown fox jumps over the lazy dog.");
+sumArray([6, 2, 1, 8, 10]) // 16
+sumArray([0, 1, 6, 10, 10]); // 17
+sumArray([ -6, -20, -1, -10, -12 ]) // -28
