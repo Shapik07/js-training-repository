51870198fe1d2763523fc527974c7254aadc2735
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-08-08 16:04:16 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -1,27 +1,14 @@
 "use strict";
 
-function sumArray(array) {
-
-  let total = 0;
-
-  if (array == null) {
-    return 0
-  } else if (array.length < 2) {
-    return 0
-  } else { 
-      const tallerNumber = Math.max(...array);
-      const smallerNumber = Math.min(...array);
-
-      for (const element of array) {
-        total += element;
-      }
-    
-     return total - tallerNumber - smallerNumber;
-  }
+function grow(x) {
+  const total = x.reduce((acc, element) => {
+    return acc * element;
+  });
+  console.log(total);
 }
-console.log(sumArray([3]));
-sumArray([6, 2, 1, 8, 10]) // 16
-sumArray([0, 1, 6, 10, 10]); // 17
-sumArray([-6, -20, -1, -10, -12]) // -28
+
+grow([1, 2, 3]) // 6
+grow([4, 1, 1, 1, 4]) // 16
+grow([2, 2, 2, 2, 2, 2]) // 64
 
 
