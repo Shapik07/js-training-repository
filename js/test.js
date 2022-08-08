a3a5a94ffa32772f270bcf8c9cec05746973a082
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-08-08 16:41:58 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -1,14 +1,13 @@
 "use strict";
 
-function grow(x) {
-  const total = x.reduce((acc, element) => {
-    return acc * element;
-  });
-  console.log(total);
-}
+function removeChar(str){
+  const array = str.split('')
+  const deleteElements = array.slice(1, -1).join('')
+  console.log(deleteElements);
+};
 
-grow([1, 2, 3]) // 6
-grow([4, 1, 1, 1, 4]) // 16
-grow([2, 2, 2, 2, 2, 2]) // 64
+removeChar("eloquent");
+removeChar("country");
+removeChar("person");
 
 
