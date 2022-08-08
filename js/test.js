5b43ded366edc02cb482ec2bc80fe2b1e9f0d4db
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-08-08 21:53:12 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -1,11 +1,18 @@
 "use strict";
 
 function opposite(number) {
-  console.log(number === 0 ? 0 : - Math.abs(number))
+  if (number === 0) {
+    console.log(0)
+  } else if (number > 0) {
+    console.log(-Math.abs(number));
+  } else {
+    console.log(Math.abs(number));
+  }
 }
 
 opposite(1);
 opposite(0);
 opposite(4.25);
+opposite(-5);
 
 
