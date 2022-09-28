9e44cfc2df2d96ca482919865959c8109ae20d1f
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-09-28 19:20:07 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -1,6 +1,50 @@
 "use strict";
 
-const animals = ['cow', 'cat', 'dog', 'elephant']
+// const animals = ['cow', 'cat', 'dog', 'elephant']
 
-console.log(animals.push('horse'))
-console.log(animals.pop())
+// console.log(animals.push('horse'))
+// console.log(animals.pop())
+
+// (() => {
+//   let a = null;
+//   if (true) {
+//     a = 7;
+//   }
+
+//   function result1() {
+//     console.log("result-1:", a);
+//   }
+
+//   function result2() {
+//     let a = 10;
+//     console.log("result-2:", a);
+
+//     result1();
+//   }
+
+//   result2();
+// })();
+
+// [].map.call('Ivan', (item) => console.log(item))
+
+// let a = {
+//   b: 3,
+//   getContext() {
+//     [1, 2, 3, 4].forEach((item) => {
+//       console.log(item);
+//       console.log(this);
+//     });
+//   },
+// };
+
+// a.getContext();
+
+let total = 0;
+const multiply = (a, b) => {
+  total = a * b;
+  console.log(a * b);
+};
+const multiply5 = (result, number) => console.log(result * number);
+
+multiply(10, 5);
+multiply5(total, 5);
