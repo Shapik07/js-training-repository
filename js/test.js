d86376a90413bc76e53ef25e200267c75e8c6729
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-09-29 21:19:17 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -1,50 +1,20 @@
 "use strict";
 
-// const animals = ['cow', 'cat', 'dog', 'elephant']
+function b(x, y, a) {
+  arguments[2] = 10;
+  alert(a);
+}
 
-// console.log(animals.push('horse'))
-// console.log(animals.pop())
+b(1, 2, 3);
 
-// (() => {
-//   let a = null;
-//   if (true) {
-//     a = 7;
-//   }
+// const numbers = [1, 2, 3, 4, 5];
+// const [y] = numbers;
+// console.log(y);
 
-//   function result1() {
-//     console.log("result-1:", a);
-//   }
-
-//   function result2() {
-//     let a = 10;
-//     console.log("result-2:", a);
-
-//     result1();
-//   }
-
-//   result2();
-// })();
-
-// [].map.call('Ivan', (item) => console.log(item))
-
-// let a = {
-//   b: 3,
-//   getContext() {
-//     [1, 2, 3, 4].forEach((item) => {
-//       console.log(item);
-//       console.log(this);
-//     });
-//   },
-// };
-
-// a.getContext();
-
-let total = 0;
-const multiply = (a, b) => {
-  total = a * b;
-  console.log(a * b);
+Number.prototype.plus = function (value) {
+  return this + value;
 };
-const multiply5 = (result, number) => console.log(result * number);
 
-multiply(10, 5);
-multiply5(total, 5);
+Number.prototype.minus = function (value) {
+  return this - value;
+};
