abdbf00097b7bed89eb4284875c10d9fea9734c8
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-10-04 23:45:08 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -3,13 +3,13 @@
 const string = "Hello i am string";
 const array = [1, 2, 3, 4, 5];
 const object = {
-    element1: 1,
-    element2: 2,
-    element3: 3,
-}
+  element1: 1,
+  element2: 2,
+  element3: 3,
+};
 
-const newString = [...string]
-const newArray = [...array]
+const newString = [...string];
+const newArray = [...array];
 const newObject = { ...object };
 
 console.log(newString);
