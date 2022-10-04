1f5c95ed0b8a96b7284c3db7141707f236bf4f7b
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-10-04 17:00:34 +0300

    Update

--- js/test.js
+++ js/test.js
@@ -1,47 +1,28 @@
 "use strict";
 
-// const tweets = [
-//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
-//   { id: "001", likes: 2, tags: ["html", "css"] },
-//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
-//   { id: "003", likes: 8, tags: ["css", "react"] },
-//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
-// ];
+const string = "Hello i am string";
+const array = [1, 2, 3, 4, 5];
+const object = {
+    element1: 1,
+    element2: 2,
+    element3: 3,
+}
 
-// const getTags = tweets =>
-//   tweets.reduce((allTags, tweet) => {
-//     allTags.push(...tweet.tags);
+const newString = [...string]
+const newArray = [...array]
+const newObject = { ...object };
 
-//     return allTags;
-//   }, []);
+console.log(newString);
+console.log(newArray);
+console.log(newObject);
 
-// const tags = getTags(tweets);
+function multiply(...args) {
+  console.log(args); // массив всех аргументов
+}
 
-// const getTagStats = (acc, tag) => {
-//   if (!acc.hasOwnProperty(tag)) {
-//     acc[tag] = 0;
-//   }
+function multiply(firstNumber, secondNumber, ...otherArgs) {
+  console.log(firstNumber); // Значение первого аргумента
+  console.log(secondNumber); // Значение второго аргумента
+  console.log(otherArgs); // Массив остальных аргументов
+}
 
-//   acc[tag] += 1;
-
-//   return acc;
-// };
-
-// // Начальное значение аккумулятора это пустой объект {}
-// const countTags = tags => tags.reduce(getTagStats, {});
-
-// const tagCount = countTags(tags);
-// console.log(tagCount);
-
-// const students = [
-//   { name: "Манго", score: 83, courses: ["математика", "физика"] },
-//   { name: "Поли", score: 59, courses: ["информатика", "математика"] },
-//   { name: "Аякс", score: 37, courses: ["физика", "биология"] },
-//   { name: "Киви", score: 94, courses: ["литература", "информатика"] },
-// ];
-
-// const sortedByAscendingScore = [...students].sort((a, b) => a.score - b.score);
-// console.log(sortedByAscendingScore);
-// const names = sortedByAscendingScore.map(student => student.name);
-
-// console.log(names); // ['Аякс', 'Поли', 'Манго', 'Киви']
